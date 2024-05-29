76d269329e59ab80d75800fd7b379da73cf81e39
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-05-29 00:02:55 -0300

    commit de casa

--- aula_03.py
+++ aula_03.py
@@ -1,5 +1,23 @@
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from time import sleep
 
+url = 'https://curso-python-selenium.netlify.app/aula_03.html'
 browser = Firefox()
 
-# mim 12:49
+browser.get(url)
+
+sleep(3)
+
+a = browser.find_element(By.TAG_NAME, 'a')
+p = browser.find_element(By.TAG_NAME, 'p')
+
+a.click()
+a.click()
+a.click()
+a.click()
+
+print(f'texto a = {a.text}')
+print(f'texto a = {p.text}')
+
+# min 52:04
